5d370890f0d8683951849eacd3bf02f1dc4618ab
Eslamgedawy <eslamgedawy93@gmail.com>
2020-05-18 05:24:32 +0200

    2 files modified

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 @Injectable()
 export class AuthService {
-
+  token: string;
   constructor() { }
 
   signUp(email: string, password: string){
@@ -15,11 +15,24 @@ export class AuthService {
   signIn(email: string, password: string){
     firebase.auth().signInWithEmailAndPassword(email,password)
        .then(
-         (response) => console.log(response)
+         response => {
+           firebase.auth().currentUser.getIdToken()
+            .then(
+              (token: string) => this.token = token
+            )
+         }
         )
         .catch(
           (error) => console.log(error)
         )
+  }
+
+  getIdToken(){
+       firebase.auth().currentUser.getIdToken()
+      .then(
+        (token: string) => this.token = token
+      );
+    return this.token;
   }
 
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,20 +4,26 @@ import { RecipeService } from '../recipes/recipe.service';
 import 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
 
-  constructor(private http: Http,private recipeService: RecipeService) { }
+  constructor(private http: Http,
+              private recipeService: RecipeService,
+              private authService: AuthService) { }
 
   // put method
 
   storeRecipes(){
-   return this.http.put('https://recipe-app-8446e.firebaseio.com/recipes.json',this.recipeService.getRecipes())
+    const token = this.authService.getIdToken();
+   return this.http.put('https://recipe-app-8446e.firebaseio.com/recipes.json?auth='+token,this.recipeService.getRecipes())
   }
 
   getRecipes(){
-    return this.http.get('https://recipe-app-8446e.firebaseio.com/recipes.json')
+    const token = this.authService.getIdToken();
+
+    return this.http.get('https://recipe-app-8446e.firebaseio.com/recipes.json?auth='+token)
       .subscribe(
           (response: Response) =>{
           console.log(response.json());
